a040590b5226dd786d25ec9ee04797da0ee0f622
lambdajon <lambdajon42@gmail.com>
2026-03-13 18:13:58 +0500

    wip: move file

--- server/src/API/Upload.hs
+++ server/src/API/Upload.hs
@@ -15,6 +15,18 @@ import GHC.Generics (Generic)
 import Servant hiding (Param)
 import Servant.Multipart
 import Servant.Server.Generic (AsServer)
+import System.Directory (renameFile)
+import System.FilePath ((</>))
+import System.FS.IO.Unix (open, getSize, close)
+import System.FS.API ( OpenMode() )
+-- import System
+-- import System.FS.IO.Unix(open, getSize, FHandle )
+-- import System.FS.API (OpenMode(..), AllowExisting (..))
+-- import System.FS.IO.Handle(HandleOS)
+-- import Data.Coerce (coerce)
+-- import System.Unsafe ()
+import System.IO (openFile, IOMode(..))
+import System.IO qualified as SIO hiding (ReadMode)
 
 type UploadRoutes :: Type -> Type
 data UploadRoutes route = MkUploadRoutes
@@ -26,11 +38,21 @@ data Report = MkReport {logs :: FilePath} deriving stock (Show)
 instance FromMultipart Tmp Report where
   fromMultipart multipartData = MkReport <$> fmap fdPayload (lookupFile "logs" multipartData)
 
+
 upload :: Report -> Handler Integer
 upload r = do
   -- we can access r.logs
+  let newPath = "/home/lambdajon/workspace/xinux/relago-support/server/data/archive.zlib"
+
+  -- let re = open r.logs ReadMode
+  -- fd <- liftIO$ open r.logs (ReadWriteMode MustExist)
+  -- fd <- liftIO$ open r.logs (ReadMode)
+
+  -- sz <- liftIO $ getSize $ fd
   liftIO $ do
+    -- renameFile (r.logs) newPath
     print r
+
   return 0
 
 uploadHandlers :: UploadRoutes AsServer
@@ -38,3 +60,6 @@ uploadHandlers =
   MkUploadRoutes
     { _log = upload
     }
+
+
+
