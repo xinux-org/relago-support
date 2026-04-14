d0a5e4935b5c050539fe4bbf31f3d80710c4a73d
lambdajon <lambdajon42@gmail.com>
2026-04-14 23:14:29 +0500

    format code

--- server/src/Search.hs
+++ server/src/Search.hs
@@ -1,9 +1,8 @@
 module Search
-  (
-    OpenSearchConfig (..)
+  ( OpenSearchConfig (..)
   , withOpenSearch
-
   ) where
+
 import Config
 import Data.Default (def)
 import Data.Text qualified as T
@@ -15,19 +14,20 @@ import Network.HTTP.Client.TLS (mkManagerSettings, newTlsManagerWith)
 
 -- | TLS settings that skip certificate verification
 insecureTlsSettings :: TLSSettings
-insecureTlsSettings = TLSSettingsSimple
-  { settingDisableCertificateValidation = True
-  , settingDisableSession = False
-  , settingUseServerName = False
-  , settingClientSupported = def
-  }
+insecureTlsSettings =
+  TLSSettingsSimple
+    { settingDisableCertificateValidation = True
+    , settingDisableSession = False
+    , settingUseServerName = False
+    , settingClientSupported = def
+    }
 
 -- | Add basic auth to requests
 addBasicAuth :: OpenSearchConfig -> Request -> Request
 addBasicAuth cfg = applyBasicAuth u p
-  where
-    u = encodeUtf8 cfg.osUser
-    p = encodeUtf8 cfg.osPassword
+ where
+  u = encodeUtf8 cfg.osUser
+  p = encodeUtf8 cfg.osPassword
 
 -- | Run OpenSearch query with auth
 withOpenSearch :: OpenSearchConfig -> BH IO a -> IO (Either EsError a)
@@ -37,6 +37,5 @@ withOpenSearch cfg action = do
       mSt = mkManagerSettings insecureTlsSettings Nothing -- FIXME: Support secure TLS
   manager <- newTlsManagerWith mSt
   let env = mkBHEnv server manager
-      envWithAuth = env { bhRequestHook = pure . addBasicAuth cfg }
+      envWithAuth = env{bhRequestHook = pure . addBasicAuth cfg}
   runBH envWithAuth action
-
