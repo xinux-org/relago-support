1dff735552ffebdc5a9e764f34c4a15b53a7573a
λjon <46736978+lambdajon@users.noreply.github.com>
2026-04-24 14:01:41 +0500

    Exchange keys (#10)
    * wip: exchange keys api
    
    * wip: ignore vendor
    
    * wip: uuid and send keyfile
    
    * Wip: setup S3
    
    * Wip: add reporters table, implement UUID type for persist
    
    * createReporter with uuid
    
    * wip: key exchange api recive publickey user and generate server side key
    
    * remove commented code

--- server/src/State.hs
+++ server/src/State.hs
@@ -3,10 +3,12 @@ module State where
 import Config
 import Data.Kind (Constraint)
 import Database.Types
+import Network.Minio (ConnectInfo)
 
 data AppSt = MkAppSt
   { config :: Config
   , db :: PoolSql
+  , s3Con :: ConnectInfo
   }
 
 type AppState :: Constraint
