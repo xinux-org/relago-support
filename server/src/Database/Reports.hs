87f516eeee47be69af3b689695cb734d96ca0faa
lambdajon <lambdajon42@gmail.com>
2026-04-24 17:13:00 +0500

    wip: get reporter by id

--- server/src/Database/Reports.hs
+++ server/src/Database/Reports.hs
@@ -2,13 +2,13 @@ module Database.Reports
   ( createReport
   , setReportIndexed
   , createReporter
+  , getReporterById
   ) where
 
 import Data.UUID (UUID)
 import Database (withPool)
-import Database.Esqueleto (runMigration)
-import Database.Persist (insert, insertKey, update, (=.))
-import Database.Types (EntityField (Indexed), Key (..), Report (..), ReportId, Reporter (..), ReporterId, migrateAll)
+import Database.Persist (get, insert, insertKey, update, (=.))
+import Database.Types (EntityField (Indexed), Key (..), Report (..), ReportId, Reporter (..))
 import Relago.Prelude
 
 createReport :: (AppState, MonadIO m) => Text -> Text -> m ReportId
@@ -19,3 +19,6 @@ setReportIndexed reportId = withPool $ update reportId [Indexed =. True]
 
 createReporter :: (AppState, MonadIO m) => UUID -> Reporter -> m ()
 createReporter uuid r = withPool $ insertKey (ReporterKey uuid) r
+
+getReporterById :: (AppState, MonadIO m) => UUID -> m (Maybe Reporter)
+getReporterById uuid = withPool $ get (ReporterKey uuid)
