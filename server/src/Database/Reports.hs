1173d6bfd8c2781c8a5c3dfc1baa75adde9aab7c
lambdajon <lambdajon42@gmail.com>
2026-04-28 11:08:20 +0500

    wip: fetching not processed reports

--- server/src/Database/Reports.hs
+++ server/src/Database/Reports.hs
@@ -1,14 +1,15 @@
 module Database.Reports
   ( createReport
   , setReportIndexed
+  , notIndexedReports
   , createReporter
   ) where
 
 import Data.UUID (UUID)
 import Database (withPool)
-import Database.Esqueleto (runMigration)
+import Database.Esqueleto.Experimental (Entity (..), from, select, table, val, where_, (==.))
 import Database.Persist (insert, insertKey, update, (=.))
-import Database.Types (EntityField (Indexed), Key (..), Report (..), ReportId, Reporter (..), ReporterId, migrateAll)
+import Database.Types (EntityField (Indexed), Key (..), Report (..), ReportId, Reporter (..))
 import Relago.Prelude
 
 createReport :: (AppState, MonadIO m) => Text -> Text -> m ReportId
@@ -16,6 +17,14 @@ createReport n fp = withPool $ insert $ Report n fp False
 
 setReportIndexed :: (AppState, MonadIO m) => ReportId -> m ()
 setReportIndexed reportId = withPool $ update reportId [Indexed =. True]
+
+notIndexedReports :: (AppState, MonadIO m) => m [(ReportId, Report)]
+notIndexedReports = withPool $ do
+  results <- select $ do
+    report <- from $ table @Report
+    where_ (report.indexed ==. val False)
+    pure report
+  pure $ (\(Entity k v) -> (k, v)) <$> results
 
 createReporter :: (AppState, MonadIO m) => UUID -> Reporter -> m ()
 createReporter uuid r = withPool $ insertKey (ReporterKey uuid) r
